cf5def3bb882e525d1cdae48270579ec99f8f2ea
Qwerty1253 <166208335+Qwerty1253@users.noreply.github.com>
2024-05-24 17:37:32 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab_24
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.RichTextBox textBoxMMB;
+        private System.Windows.Forms.RichTextBox textBoxAR;
+        private System.Windows.Forms.RichTextBox textBoxPles;
+        private System.Windows.Forms.Button buttonStartMMB;
+        private System.Windows.Forms.Button buttonStopMMB;
+        private System.Windows.Forms.Button buttonStartAR;
+        private System.Windows.Forms.Button buttonStopAR;
+        private System.Windows.Forms.Button buttonStartPles;
+        private System.Windows.Forms.Button buttonStopPles;
+        private System.Windows.Forms.Button buttonStartAll;
+        private System.Windows.Forms.Button buttonStopAll;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxMMB = new System.Windows.Forms.RichTextBox();
+            this.textBoxAR = new System.Windows.Forms.RichTextBox();
+            this.textBoxPles = new System.Windows.Forms.RichTextBox();
+            this.buttonStartMMB = new System.Windows.Forms.Button();
+            this.buttonStopMMB = new System.Windows.Forms.Button();
+            this.buttonStartAR = new System.Windows.Forms.Button();
+            this.buttonStopAR = new System.Windows.Forms.Button();
+            this.buttonStartPles = new System.Windows.Forms.Button();
+            this.buttonStopPles = new System.Windows.Forms.Button();
+            this.buttonStartAll = new System.Windows.Forms.Button();
+            this.buttonStopAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMMB
+            // 
+            this.textBoxMMB.Location = new System.Drawing.Point(12, 12);
+            this.textBoxMMB.Name = "textBoxMMB";
+            this.textBoxMMB.Size = new System.Drawing.Size(433, 202);
+            this.textBoxMMB.TabIndex = 0;
+            this.textBoxMMB.Text = "";
+            // 
+            // textBoxAR
+            // 
+            this.textBoxAR.Location = new System.Drawing.Point(12, 220);
+            this.textBoxAR.Name = "textBoxAR";
+            this.textBoxAR.Size = new System.Drawing.Size(433, 196);
+            this.textBoxAR.TabIndex = 1;
+            this.textBoxAR.Text = "";
+            // 
+            // textBoxPles
+            // 
+            this.textBoxPles.Location = new System.Drawing.Point(12, 422);
+            this.textBoxPles.Name = "textBoxPles";
+            this.textBoxPles.Size = new System.Drawing.Size(433, 200);
+            this.textBoxPles.TabIndex = 2;
+            this.textBoxPles.Text = "";
+            // 
+            // buttonStartMMB
+            // 
+            this.buttonStartMMB.Location = new System.Drawing.Point(489, 109);
+            this.buttonStartMMB.Name = "buttonStartMMB";
+            this.buttonStartMMB.Size = new System.Drawing.Size(100, 30);
+            this.buttonStartMMB.TabIndex = 3;
+            this.buttonStartMMB.Text = "Start MMB";
+            // 
+            // buttonStopMMB
+            // 
+            this.buttonStopMMB.Enabled = false;
+            this.buttonStopMMB.Location = new System.Drawing.Point(622, 109);
+            this.buttonStopMMB.Name = "buttonStopMMB";
+            this.buttonStopMMB.Size = new System.Drawing.Size(100, 30);
+            this.buttonStopMMB.TabIndex = 4;
+            this.buttonStopMMB.Text = "Stop MMB";
+            // 
+            // buttonStartAR
+            // 
+            this.buttonStartAR.Location = new System.Drawing.Point(489, 292);
+            this.buttonStartAR.Name = "buttonStartAR";
+            this.buttonStartAR.Size = new System.Drawing.Size(100, 30);
+            this.buttonStartAR.TabIndex = 5;
+            this.buttonStartAR.Text = "Start AR";
+            // 
+            // buttonStopAR
+            // 
+            this.buttonStopAR.Enabled = false;
+            this.buttonStopAR.Location = new System.Drawing.Point(622, 292);
+            this.buttonStopAR.Name = "buttonStopAR";
+            this.buttonStopAR.Size = new System.Drawing.Size(100, 30);
+            this.buttonStopAR.TabIndex = 6;
+            this.buttonStopAR.Text = "Stop AR";
+            // 
+            // buttonStartPles
+            // 
+            this.buttonStartPles.Location = new System.Drawing.Point(489, 514);
+            this.buttonStartPles.Name = "buttonStartPles";
+            this.buttonStartPles.Size = new System.Drawing.Size(100, 30);
+            this.buttonStartPles.TabIndex = 7;
+            this.buttonStartPles.Text = "Start Ples";
+            // 
+            // buttonStopPles
+            // 
+            this.buttonStopPles.Enabled = false;
+            this.buttonStopPles.Location = new System.Drawing.Point(622, 514);
+            this.buttonStopPles.Name = "buttonStopPles";
+            this.buttonStopPles.Size = new System.Drawing.Size(100, 30);
+            this.buttonStopPles.TabIndex = 8;
+            this.buttonStopPles.Text = "Stop Ples";
+            // 
+            // buttonStartAll
+            // 
+            this.buttonStartAll.Location = new System.Drawing.Point(740, 174);
+            this.buttonStartAll.Name = "buttonStartAll";
+            this.buttonStartAll.Size = new System.Drawing.Size(133, 64);
+            this.buttonStartAll.TabIndex = 9;
+            this.buttonStartAll.Text = "Start All";
+            // 
+            // buttonStopAll
+            // 
+            this.buttonStopAll.Location = new System.Drawing.Point(740, 380);
+            this.buttonStopAll.Name = "buttonStopAll";
+            this.buttonStopAll.Size = new System.Drawing.Size(133, 67);
+            this.buttonStopAll.TabIndex = 10;
+            this.buttonStopAll.Text = "Stop All";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(884, 644);
+            this.Controls.Add(this.textBoxMMB);
+            this.Controls.Add(this.textBoxAR);
+            this.Controls.Add(this.textBoxPles);
+            this.Controls.Add(this.buttonStartMMB);
+            this.Controls.Add(this.buttonStopMMB);
+            this.Controls.Add(this.buttonStartAR);
+            this.Controls.Add(this.buttonStopAR);
+            this.Controls.Add(this.buttonStartPles);
+            this.Controls.Add(this.buttonStopPles);
+            this.Controls.Add(this.buttonStartAll);
+            this.Controls.Add(this.buttonStopAll);
+            this.Name = "Form1";
+            this.Text = "Crypto Algorithms Simulator";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab_24
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource[] cancellationTokenSources = new CancellationTokenSource[3];
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeEventHandlers();
+        }
+
+        private void InitializeEventHandlers()
+        {
+            buttonStartMMB.Click += (sender, e) => StartCryptoProcess(0, MMBAlgorithm, "MMB");
+            buttonStartAR.Click += (sender, e) => StartCryptoProcess(1, ARHashFunction, "AR Hash");
+            buttonStartPles.Click += (sender, e) => StartCryptoProcess(2, PlesseyGenerator, "Plessey");
+
+            buttonStopMMB.Click += (sender, e) => StopCryptoProcess(0);
+            buttonStopAR.Click += (sender, e) => StopCryptoProcess(1);
+            buttonStopPles.Click += (sender, e) => StopCryptoProcess(2);
+
+            buttonStartAll.Click += (sender, e) => {
+                StartCryptoProcess(0, MMBAlgorithm, "MMB");
+                StartCryptoProcess(1, ARHashFunction, "AR Hash");
+                StartCryptoProcess(2, PlesseyGenerator, "Plessey");
+            };
+
+            buttonStopAll.Click += (sender, e) => {
+                for (int i = 0; i < 3; i++) StopCryptoProcess(i);
+            };
+        }
+
+        private void StartCryptoProcess(int index, Func<string, Task<string>> cryptoMethod, string algorithmName)
+        {
+            if (cancellationTokenSources[index] != null && !cancellationTokenSources[index].IsCancellationRequested)
+                return;
+
+            cancellationTokenSources[index] = new CancellationTokenSource();
+            var token = cancellationTokenSources[index].Token;
+
+            Control startButton = GetStartButton(index);
+            Control stopButton = GetStopButton(index);
+            RichTextBox outputBox = GetOutputBox(index);
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+
+            Task.Run(async () => {
+                while (!token.IsCancellationRequested)
+                {
+                    string input = GenerateRandomString(8);  // Генерируем случайную строку длиной 8 символов
+                    string result = null;
+
+                    try
+                    {
+                        result = await cryptoMethod(input);
+
+                        if (token.IsCancellationRequested)
+                        {
+                            Invoke(new Action(() => {
+                                AppendText(outputBox, Environment.NewLine + "Process was cancelled before updating result." + Environment.NewLine, Color.Red);
+                            }));
+                            break;
+                        }
+
+                        Invoke(new Action(() => {
+                            AppendText(outputBox, algorithmName + ": ", Color.Blue);
+                            AppendText(outputBox, input, Color.Green);
+                            AppendText(outputBox, " -> ", Color.Black);
+                            AppendText(outputBox, result + Environment.NewLine, Color.Red);
+                            ScrollToBottom(outputBox);
+                        }));
+                    }
+                    catch (Exception ex)
+                    {
+                        Invoke(new Action(() => {
+                            AppendText(outputBox, $"Error: {ex.Message}" + Environment.NewLine, Color.Red);
+                            ScrollToBottom(outputBox);
+                        }));
+                    }
+
+                    await Task.Delay(1000); // Simulate some processing time to see the UI updates
+                }
+
+                Invoke(new Action(() => {
+                    startButton.Enabled = true;
+                    stopButton.Enabled = false;
+                    UpdateAllButtonsState();
+                }));
+            }, token);
+
+            UpdateAllButtonsState();
+        }
+
+        private void StopCryptoProcess(int index)
+        {
+            if (cancellationTokenSources[index] != null)
+            {
+                cancellationTokenSources[index].Cancel();
+                cancellationTokenSources[index].Dispose();
+                cancellationTokenSources[index] = null;
+            }
+
+            GetStopButton(index).Enabled = false;
+            GetStartButton(index).Enabled = true;
+
+            UpdateAllButtonsState();
+        }
+
+        private Task<string> MMBAlgorithm(string input)
+        {
+            // Simulate MMB algorithm (for demonstration purposes)
+            using (var sha256 = SHA256.Create())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var result = sha256.ComputeHash(inputBytes);
+                return Task.FromResult(BitConverter.ToString(result).Replace("-", ""));
+            }
+        }
+
+        private Task<string> ARHashFunction(string input)
+        {
+            // Simulate AR hash function (for demonstration purposes)
+            using (var md5 = MD5.Create())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var result = md5.ComputeHash(inputBytes);
+                return Task.FromResult(BitConverter.ToString(result).Replace("-", ""));
+            }
+        }
+
+        private Task<string> PlesseyGenerator(string input)
+        {
+            // Simulate Plessey generator (for demonstration purposes)
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var resultBytes = new byte[inputBytes.Length];
+                rng.GetBytes(resultBytes);
+                return Task.FromResult(BitConverter.ToString(resultBytes).Replace("-", ""));
+            }
+        }
+
+        private string GenerateRandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var stringChars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(stringChars);
+        }
+
+        private void UpdateAllButtonsState()
+        {
+            bool anyRunning = false;
+            bool allRunning = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (cancellationTokenSources[i] != null && !cancellationTokenSources[i].IsCancellationRequested)
+                {
+                    anyRunning = true;
+                }
+                else
+                {
+                    allRunning = false;
+                }
+            }
+
+            buttonStartAll.Enabled = !allRunning;
+            buttonStopAll.Enabled = anyRunning;
+        }
+
+        // Helper methods to get the controls by index
+        private System.Windows.Forms.Button GetStartButton(int index)
+        {
+            switch (index)
+            {
+                case 0: return buttonStartMMB;
+                case 1: return buttonStartAR;
+                case 2: return buttonStartPles;
+                default: throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        private System.Windows.Forms.Button GetStopButton(int index)
+        {
+            switch (index)
+            {
+                case 0: return buttonStopMMB;
+                case 1: return buttonStopAR;
+                case 2: return buttonStopPles;
+                default: throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        private System.Windows.Forms.RichTextBox GetOutputBox(int index)
+        {
+            switch (index)
+            {
+                case 0: return textBoxMMB;
+                case 1: return textBoxAR;
+                case 2: return textBoxPles;
+                default: throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        private void AppendText(System.Windows.Forms.RichTextBox textBox, string text, Color color)
+        {
+            textBox.SelectionStart = textBox.TextLength;
+            textBox.SelectionLength = 0;
+
+            textBox.SelectionColor = color;
+            textBox.AppendText(text);
+            textBox.SelectionColor = textBox.ForeColor;
+        }
+
+        private void ScrollToBottom(System.Windows.Forms.RichTextBox textBox)
+        {
+            textBox.SelectionStart = textBox.Text.Length;
+            textBox.ScrollToCaret();
+        }
+    }
+}
